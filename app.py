f74be62668069818a36a8caf73723845113bb885
Rudra <rudranarayan.muduli@kemsys.com>
2023-08-18 15:44:38 +0530

    add new design and feature

--- app.py
+++ app.py
@@ -12,32 +12,31 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/download', methods=['POST'])
+@app.route('/download', methods=['GET', 'POST'])
 def download():
-    try:
-        video_url = request.form.get('video_url')
-        new_directory = 'downloads'
-        os.makedirs(new_directory, exist_ok=True)
+    if request.method == 'POST':
+            video_url = request.form.get('video_url')
+            new_directory = 'downloads'
+            os.makedirs(new_directory, exist_ok=True)
 
-        yt = YouTube(video_url)
-        video_stream = yt.streams.get_highest_resolution()
+            yt = YouTube(video_url)
+            video_stream = yt.streams.get_highest_resolution()
 
-        # Clean up the video title using regex
-        cleaned_title = re.sub(r'[^\w\s-]', '', yt.title)
-        cleaned_title = re.sub(r'\s+', '-', cleaned_title)
+            # Clean up the video title using regex
+            cleaned_title = re.sub(r'[^\w\s-]', '', yt.title)
+            cleaned_title = re.sub(r'\s+', '-', cleaned_title)
 
-        filename = cleaned_title + '.mp4'
-        video_stream.download(output_path=new_directory, filename=filename)
+            filename = cleaned_title + '.mp4'
+            video_stream.download(output_path=new_directory, filename=filename)
 
-        # Send the file as an attachment
-        response = send_file(os.path.join(new_directory, filename), as_attachment=True)
+            # Send the file as an attachment
+            response = send_file(os.path.join(new_directory, filename), as_attachment=True)
 
-        # Schedule the deletion of the downloaded video after 10 minutes
-        schedule.every(10).minutes.do(delete_video, filename)
+            # Schedule the deletion of the downloaded video after 10 minutes
+            schedule.every(10).minutes.do(delete_video, filename)
 
-        return response
-    except Exception as e:
-        return str(e)
+            return response
+    return render_template('videoDownloader.html')
 
 def delete_video(filename):
     downloads_dir = 'downloads'
@@ -53,6 +52,10 @@ def schedule_cleanup():
     while True:
         schedule.run_pending()
         time.sleep(1)
+
+@app.route('/PrivacyPolicy')
+def privacy():
+    return render_template('privacy.html')        
 
 if __name__ == '__main__':
     app.run(debug=True)
